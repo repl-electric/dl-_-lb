169ee75f30747982ee734568fb5f9acf4e8d43c2
Joseph Wilk <joe@josephwilk.net>
2018-02-14 11:14:26 +0000

    Alias for samples

---
+++ you_fall_into_your_screen/sample_map.rb
@@ -0,0 +1,35 @@
+KT = Lo[/kick/,/thin/]
+KK = Lo[/kick/,/clicky/]
+K1,K2,K3,K4=Frag[/kick/,9], Mountain[/subkick/,0], Mountain[/kick/,4], Lo[/kick/,15]
+KK = Lo[/kick/,/clicky/]
+KD = Lo[/kick/,/dark/]
+KP = Lo[/kick/,/phat/]
+KM = Lo[/kick/,/mid/]
+
+P = Dust[/perc/, /conga/]
+T1 = Tape[/tom/,24]
+TC = Tape[/cymbals/]
+TS = Tape[/snares/]
+TT = Tape[/toms/]
+DF = Drip[/fx/]
+M1 = Mountain[/microperc/]
+F1 = ["/Users/josephwilk/Workspace/music/samples/Tuned/Drip_effects/D-01.wav", "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_effects/D01.wav", "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_effects/E01.wav"][2]
+
+CC = Crystal[/cymbal/]
+CF = Crystal[/fx/]
+C1 = Analog[/clapraw/,0]
+C2 = Analog[/04 clap1/,0]
+C3 = Analog[/04 clap2/,0]
+C4 = Analog[/04 clap3/,0]
+C5 = Tape[/cymbal/,7]
+C6 = Analog[/13 clap/,1]
+C7 = Analog[/13 clap/,2]
+
+D1,D2,D3,D4 = *Drip[/cymbal/,[0,2,3,4]]
+D1c = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/c#01.wav"
+D1d = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/d01.wav"
+D1e = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/e01.wav"
+D1f = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/f#01.wav"
+D1g = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/g#01.wav"
+D1a = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/a-01.wav"
+D1b = "/Users/josephwilk/Workspace/music/samples/Tuned/Drip_cymbals/b01.wav";
