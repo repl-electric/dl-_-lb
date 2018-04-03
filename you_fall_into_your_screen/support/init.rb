16bc0460a5edd3014d7b06a1e976cd1a51e709b7
Joseph Wilk <joe@josephwilk.net>
2018-04-03 20:12:10 +0100

    MORE OSC MESSAGES!

--- you_fall_into_your_screen/support/init.rb
+++ you_fall_into_your_screen/support/init.rb
@@ -16,6 +16,7 @@
 #unity "/alive/maxtime", 0.006
 unity "/postfx",0.0
 def camlogo(r=nil)
+  viz :alive, color: 0.0
   unity "/alive/light", 0.3
   unity "/logo/re",0.0
   unity "/logo/person", 0.0
@@ -60,6 +61,8 @@ def dcamend(r=nil)
 end
 
 def cam0
+  $mode = 0
+  viz :alive, color: 0.108
   unity "/alive/light", 0.9
   unity "/sea/spacex", 0.1
   unity "/sea/height", 1.3
@@ -78,9 +81,11 @@ def cam0
   viz :sea, color: 0.0
   viz :sea, size: 1.0
   viz :alive, deformrate: 0.0
+  viz :alive, gravity: 1, amp: 1, freq: 1, speed: 1
 end
 
 def cam1
+  $mode = 1
   unity "/alive/light", 0.7
   unity "/alive/rotate", 0.0
   unity "/alive/thick",0.0144
@@ -95,12 +100,16 @@ def cam1
 end
 
 def cam2
+  $mode = 2
   viz :alive, light: 0.6
   viz camera: 1
   #viz :alive, deform: 300.0
 end
 
 def cam3
+  $mode = 3
+  @light=0.6
+  viz :alive, color: 0.0
   viz :alive, light: 0.6
   viz camera: 3
   unity "/alive/thick",0.0144
@@ -117,12 +126,25 @@ def crashcam3(factor=0.0)
   viz :glitch, crash: 0.85*factor
 end
 
+def terrain(height=0.0)
+  if height
+    viz :alive, terrain: 1.0
+    viz :alive, height: height
+    viz :alive, x: 15.49
+    viz :alive, z: 9.79
+  else
+    viz :alive, terrain: 0.0
+  end
+end
+
 def cam4
   $mode = 4
+  viz :alive, gravity: 1, amp: 1, freq: 1, speed: 1
   viz :alive, light: 0.6
   viz camera: 4
   unity "/alive/thick",0.0144
   unity "/alive/length",0.06
+  viz :sea, on: 0.0
   viz :glitch, color: 0.0
   @thick = 0.01
   viz :logo, blank: 1.0
