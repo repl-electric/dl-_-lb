4d563016cf7b0b971db71d534748e104532ee6f9
Joseph Wilk <joe@josephwilk.net>
2018-05-09 15:48:12 +0100

    practice visuals

---
+++ piezoelectric_resonator/scratch/graphics.rb
@@ -0,0 +1,53 @@
+unity "/explode1"
+#unity "/explode2"
+#unity "/explode3"
+
+#unity "/"
+stop
+#unity "/scene/1",1
+#at{
+#  unity "/rockcircle/rot", 100.0
+#  unity "/rockcircle/orbit", 5.0
+#  unity "/rockcircle/y", 0.5
+ # sleep 0.125
+#  unity "/rockcircle/y", 0.0
+ # }
+
+
+#unity "/rockcircle/noise", 1.5
+#unity "/rockcircle/freq", 0.2
+
+
+
+
+#stop
+
+#stop
+
+#midi_cc 52, 1
+#stop
+ unity "/sea/rippleheight", 0.5
+ unity "/sea/waveheight", 4.5
+#  unity "/rocks",1.0
+#  unity "/rocks/vortex",1.0
+#  unity "/rocks/turb",0.0
+#  unity "/rocks/vortex/radius",5.0
+#  unity "/rocks/vortex/force", -50.0
+#  unity "/rocks/pos", 0.2
+# unity "/burst", 0.01
+#unity "/roots",1.0
+#unity "/roots/drag", 2.0
+#unity "/roots/freq", 0.15
+#unity "/bird/hover", 1.0
+#unity "/scale",20.0
+#unity "/rockcircle/y", -10.0
+#unity "/rockcircle/orbit", 5.0
+#unity "/rockcircle/pos", 4.0
+#unity "/rockcircle/rot", 0.0
+
+
+stop
+
+
+unity "/sea/rippleheight", 0.5
+unity "/sea/waveheight", 6.8
