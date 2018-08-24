cc2d23b6ca9a54e7571bfb94a40b11afb8d0cb33
Joseph Wilk <joe@josephwilk.net>
2018-08-24 15:06:09 +0100

    setup start state

---
+++ dflipflop/support/init.rb
@@ -0,0 +1,6 @@
+def init!
+  alive dragon: 0, hpad: 0, spad: 1, glitch: 0, bass: 0, derbass: 0, twinebass: 0, wpiano: 1, piano: 1, piano_echos: 1, pluck: 0
+  spad_cc filter: 0.15*1
+  wpiano_cc wash: 0.1
+  pluck_cc wash: 0.2
+end
