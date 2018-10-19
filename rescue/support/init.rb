70126b92da4833bee8c85c6572271ed3123f74b2
Joseph Wilk <joe@josephwilk.net>
2018-10-19 12:48:28 +0100

    unity connection

--- rescue/support/init.rb
+++ rescue/support/init.rb
@@ -2,3 +2,7 @@ def init!
   zero_cc wash: 0.5
   alive kick: 0, crystal: 0, perc: 0, piano: 0, vocal: 0, kalim: 1, kalim2: 0, harp: 0,  pad: 0, vastness: 0, waves: 0
 end
+
+def scene(n)
+  unity "/scene/#{n}"
+end
