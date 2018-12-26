011696afe54753ea7712882fc8976d8018927f70
Joseph Wilk <joe@josephwilk.net>
2018-12-26 03:15:15 +0000

    working on aura entrance

--- piezoelectric_resonator/support/init.rb
+++ piezoelectric_resonator/support/init.rb
@@ -189,6 +189,7 @@ def create_aura(n=0)
   unity "/aura",n
   unity "/cube/aura/globalscale", 0.0
   unity "/cube/aura/scalemul", -0.6
+
 end
 def world(*args)
   opts = resolve_synth_opts_hash_or_array(args)
@@ -714,7 +715,7 @@ def init(force=false)
     rocksinit
     rocks throttle: 1
     vortex y: 1.25, throttle: 0.2, turb: 0, force: 0
-    cube aura: 1.47
+    #cube aura: 1.47
     cube rot: 1
     cam :cube
     roots_init
@@ -727,18 +728,17 @@ def init(force=false)
     unity "/cube/hit", 0.0
     star throttle: 0.0
     star size: 0.0000001, life: -100.000001
-    cube aura: 1.47
-
+    #cube aura: 1.47
 
     colorb 0
-
-    sleep 2
-    #create_aura -0.9
     unity "/cube/aura/globalscale", 0.0
     unity "/cube/aura/scalemul", -0.6
-    create_aura -2
-    aura fresnel: 1.5, scale: 0, wave: 0.01
-    unity "/cube/aura/scalemul", -0.99
+
+#    sleep 2
+    create_aura -0.1
+    aura fresnel: 1.5, scale: 0.0, wave: 0.01
+
+    #unity "/cube/aura/scalemul", -0.99
     at{
       sleep 0.5
       rocks speed: 0.01, orbit: 0.0
